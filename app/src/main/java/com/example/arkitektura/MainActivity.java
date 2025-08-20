6be515f3fb981997a3307ae2b10d27e9c8586332
signetK <gabrieljradriano@gmail.com>
2025-08-21 00:07:32 +0800

    Header, Bottom Navigation.

--- app/src/main/java/com/example/arkitektura/MainActivity.java
+++ app/src/main/java/com/example/arkitektura/MainActivity.java
@@ -8,17 +8,54 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.arkitektura.fragments.Home;
+import com.example.arkitektura.fragments.AboutUs;
+import com.example.arkitektura.fragments.Content;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setOnItemSelectedListener(navListener);
+
+        Fragment selectedFragment = new Home();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+
+    private BottomNavigationView.OnItemSelectedListener navListener = item -> {
+        int ItemId = item.getItemId();
+
+        Fragment selectedFragment = null;
+
+        if (ItemId == R.id.home) {
+        selectedFragment = new Home();
+        } else if (ItemId == R.id.about_us) {
+        selectedFragment = new AboutUs();
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+        return true;
+    };
 }
