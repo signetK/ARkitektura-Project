1d4a31f3d320c6d05e3e9deacb8a39ce9ad6fe1e
signetK <gabrieljradriano@gmail.com>
2025-08-21 19:34:40 +0800

    WIP Home

--- app/src/main/java/com/example/arkitektura/adapter/CollegeAdapter.java
+++ app/src/main/java/com/example/arkitektura/adapter/CollegeAdapter.java
@@ -8,10 +8,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.arkitektura.R;
 import com.example.arkitektura.model.CollegeModel;
+import com.example.arkitektura.fragments.Content;
 
 import java.util.List;
 
@@ -37,6 +39,16 @@ public class CollegeAdapter extends RecyclerView.Adapter<CollegeAdapter.CollegeV
         holder.collegeImage.setImageResource(college.getImageRes());
         holder.collegeLogo.setImageResource(college.getLogoRes());
         holder.collegeName.setText(college.getName());
+
+        holder.collegeCard.setOnClickListener(v -> {
+            Content content = Content.newInstance(college.getName(), "Some other param");
+
+            ((androidx.fragment.app.FragmentActivity)v.getContext()).getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, content)
+                    .addToBackStack(null)
+                    .commit();
+        });
     }
 
     @Override
@@ -45,11 +57,15 @@ public class CollegeAdapter extends RecyclerView.Adapter<CollegeAdapter.CollegeV
     }
 
     static class CollegeViewHolder extends RecyclerView.ViewHolder {
+
+        CardView collegeCard;
         ImageView collegeImage, collegeLogo;
+
         TextView collegeName;
 
         public CollegeViewHolder(@NonNull View itemView) {
             super(itemView);
+            collegeCard = itemView.findViewById(R.id.collegeCard);
             collegeImage = itemView.findViewById(R.id.collegeImage);
             collegeLogo = itemView.findViewById(R.id.collegeLogo);
             collegeName = itemView.findViewById(R.id.collegeName);
